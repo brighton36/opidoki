d0c1479ec33e2744c5660489cf57f55bc7583b3f
sion2k <sionronin@gmail.com>
2015-01-10 10:35:21 -0500

    Nav, header and form fleshed out

--- app/controllers/broadcasts_controller.rb
+++ app/controllers/broadcasts_controller.rb
@@ -2,4 +2,18 @@ class BroadcastsController < ApplicationController
   def index
     @broadcast = Broadcast.new
   end
+
+  def create
+    Broadcast.create(broadcast_params)
+  end
+
+  def update
+    #strong_params
+  end
+
+  private
+
+  def broadcast_params 
+    params.require(:broadcast).permit(:label, :closes_at, :creator, :btc_public_address, :match_type, :url, :match_javascript, :match_regex, :include_jquery)
+  end
 end
