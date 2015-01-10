e565fc78d574e2cf3cd9115a040b7a45523d8394
Chris DeRose <chris@chrisderose.com>
2015-01-10 16:17:05 +0000

    Added some scopes to the model

--- app/models/broadcast.rb
+++ app/models/broadcast.rb
@@ -11,4 +11,18 @@ document.getElementsByTagName('head')[0].appendChild(jq);
 $.noConflict();
 eos
 
+  # The address has sufficient balance to open/close:
+  scope :funded, lambda{ where( 'is_funded = ?', true) }
+
+  # Open Broadcast is sent:
+  scope :opened, lambda{ where( 'is_opened = ?', true) }
+
+  # Close Broadcast is sent:
+  scope :closed, lambda{ where( 'is_closed = ?', true) }
+
+  # These are in the queue and ready to close:
+  scope :upcoming, lambda{ |now| funded.opened.where('closes_at >= ?', now) }
+
+  # These have succesfully completed
+  scope :expired, lambda{ |now| funded.closed.where('closes_at <= ?', now) }
 end
