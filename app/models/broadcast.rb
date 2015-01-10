791991a2f875c2b1a8af9d30d5db21b3948f3ce3
Chris DeRose <chris@chrisderose.com>
2015-01-10 14:47:53 +0000

    Model updates

--- app/models/broadcast.rb
+++ app/models/broadcast.rb
@@ -1,3 +1,14 @@
 class Broadcast < ActiveRecord::Base
+  # NOTE: We should probably be using an enum here, but it's a hackathon
+  MATCH_TYPE_REGEX = 1
+  MATCH_TYPE_JAVASCRIPT = 2
+
+  INCLUDE_JQUERY_SCRIPT = <<eos
+// Init Jquery:
+var jq = document.createElement('script');
+jq.src = "https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js";
+document.getElementsByTagName('head')[0].appendChild(jq);
+$.noConflict();
+eos
 
 end
